bc17d245af600ded7944cc7be14c7ffc68adcdea
Artyom <arkrasovski1996@gmail.com>
2021-03-08 18:39:32 +0300

    change design

--- time.js
+++ time.js
@@ -1,37 +1,30 @@
-﻿function privet() {
-    var timer1 = new Date();
-    var timer_hours1 = timer1.getHours();
+﻿const welcome = document.querySelector(".time .greeting");
+console.log(welcome);
+const message = ", уважаемый пользователь!";
+function hello() {
+  var timer = new Date();
+  var timer_hours = timer.getHours();
 
-    if (timer_hours1 > 18 && timer_hours1 < 24) {
-        confirm('good evening');
-    }
-    else if (timer_hours1 > 6 && timer_hours1 < 12) {
-        confirm('good morning');
-    }
-    else if (timer_hours1 > 12 && timer_hours1 < 18) {
-        confirm('good afternoon');
-    }
-    else if (timer_hours1 >= 0 && timer_hours1 < 6) {
-        confirm('good night');
-    };
-};
-privet();
-var obj_hours = document.getElementById("timer_content");
+  if (timer_hours >= 18 && timer_hours < 24) {
+    welcome.innerHTML = "Добрый вечер" + message;
+  } else if (timer_hours > 6 && timer_hours < 12) {
+    welcome.innerHTML = "Доброе утро" + message;
+  } else if (timer_hours > 12 && timer_hours < 18) {
+    welcome.innerHTML = "Добрый день" + message;
+  } else if (timer_hours >= 0 && timer_hours < 6) {
+    welcome.innerHTML = "Доброй ночи" + message;
+  }
+}
+hello();
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
 
-function wr_hours() {
-    var time = new Date();
-    var time_hours = time.getHours();
-    var time_min = time.getMinutes();
-    var time_sec = time.getSeconds();
-    var full_time = 'Сейчас ' + ((time_hours < 10) ? "0" : "") + time_hours;
-    full_time += ":";
-    full_time += ((time_min < 10) ? "0" : "") + time_min;
-    full_time += ":";
-    full_time += ((time_sec < 10) ? "0" : "") + time_sec;
+function timer() {
+  var time = new Date();
+  var time_hours = time.getHours();
+  var time_min = time.getMinutes();
+  hours.innerHTML = (time_hours < 10 ? "0" : "") + time_hours;
+  minutes.innerHTML = (time_min < 10 ? "0" : "") + time_min;
+}
 
-    obj_hours.innerHTML = full_time;
-
-};
-
-
-setInterval("wr_hours();", 1000);
+setInterval("timer();", 1000);
